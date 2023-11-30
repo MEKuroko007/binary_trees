d9e2d6295c1b7cd8dffce75d8b6c12040a11225a
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-11-30 19:45:36 +0100

    fix issues of right&left leaf tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,24 +13,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left == NULL)
-	{
-		parent->left = new_node;
-	}
-	else
+	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		parent->left = new_node;
 	}
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->right = NULL;
+	parent->left = new_node;
 
 	return (new_node);
 }
