8a1bad09f3d57b0ca8d982bf8e86156fbc1ca620
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-11-30 20:31:45 +0100

    handle case 0 in height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to tree
- * Return: height
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: Height of the tree, 0 if tree is NULL.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	size_t left_height = tree->left ? (1 + binary_tree_height(tree->left)) : 0;
+	size_t right_height = tree->right ? (1 + binary_tree_height(tree->right)) : 0;
 
-	return (1 + ((left_height > right_height) ? left_height : right_height));
+	return ((left_height > right_height) ? left_height : right_height);
 }
